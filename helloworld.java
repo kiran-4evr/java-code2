6b6fa9e092774ccf2da82c64bd145b3780143eec
Kiran Kumar <kiran.4evr@gmail.com>
2018-08-26 04:51:23 +0000

    i am on first branch

--- helloworld.java
+++ helloworld.java
@@ -22,6 +22,6 @@ public class CallingMethodsInSameClass
 		printOne();
               System.out.println("this is first brach");
               System.out.println("Hi hello");
-              System.out.println("Hi How are you");
+//              System.out.println("Hi How are you");
 	}
 }
